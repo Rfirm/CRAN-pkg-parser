a395df626a378ccad678df7b3d62d1d09e56d858
howardchi <howardchi@fougood.com>
2014-04-27 14:58:41 +0800

    init done

---
+++ README.md
@@ -0,0 +1,19 @@
+# CRAN-pkg-parser
+
+A package parser for CRAN site.
+
+## Develop
+
+```
+npm install
+```
+Start curl data from origin:
+
+```
+node curl.js
+```
+after this the data will saved to file `data/pkg.json`
+
+## License
+
+MIT [@chilijung](http://github.com/chilijung)
---
+++ curl.js
@@ -0,0 +1,59 @@
+var request = require('request');
+var stage = require('node-stage');
+var jsdom = require('jsdom');
+var mkdirp = require('mkdirp');
+var fs = require('fs');
+
+// origin: http://mirror.fcaglp.unlp.edu.ar/CRAN/web/packages/available_packages_by_date.html
+
+var origin = 'http://mirror.fcaglp.unlp.edu.ar/CRAN/web/packages/available_packages_by_date.html';
+
+request.get({
+	uri: origin
+}, function(err, res, body) {
+	if(!err && res.statusCode == 200) {
+		create_fs();
+		jsdom.env(
+			body,
+			["http://code.jquery.com/jquery.min.js"],
+			function (errors, window) {
+				var $ = window.$;
+				var pkg_arr = [];
+				$("table > tr").each(function(i) {
+					var pkg_obj = {};
+					if(i > 1) {
+						$(this).children('td').each(function(i) {
+							var $inner = $(this).html();
+							if(i === 0) {
+								// date 
+								pkg_obj.date = $inner;
+							}else if (i === 1) {
+								// package
+								pkg_obj.package = $inner;
+							}else if (i === 2) {
+								// title
+								pkg_obj.title = $inner;
+							}
+						})
+						pkg_arr.push(pkg_obj);
+					}
+				})
+				fs.writeFileSync('./data/pkg.json', JSON.stringify(pkg_arr))
+				stage.success('DONE!!!!!!!!!!!')
+			}
+		);
+    }else {
+      stage.error('Request error!')
+    } 
+})
+
+// create folder if is not exist
+function create_fs() {
+  mkdirp(__dirname + '/data/', function (err) {
+    if (err) {
+      stage.error(err)
+    } else { 
+      stage.process('success create folder: ./data/')
+    }
+  })
+}
